b3d63fd58c738987a7a8878cc01ee807388c0071
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2024-12-04 09:42:55 +0000

    Update start-node-prod.sh

--- conf/entrypoint/start-node-prod.sh
+++ conf/entrypoint/start-node-prod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# pm2 will restart the node process on crashed, `--no-daemon` will attach to application log
+# pm2 will restart the node process on unhandled errors, `--no-daemon` will attach to application log
 pm2 start /usr/local/bin/node/server.js --no-daemon &
