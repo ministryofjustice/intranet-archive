27f2bb01de809506d071cb401c0cd50ba90230cd
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2025-03-10 14:58:48 +0000

    Fix empty metrics error (#89)

--- conf/node/controllers/metrics.js
+++ conf/node/controllers/metrics.js
@@ -149,7 +149,6 @@ export const getAllMetrics = async (envs) => {
     }
   }
 
-
   return metrics;
 };
 
@@ -167,6 +166,17 @@ export const getMetricsString = (metrics) => {
   Object.entries(metricsProperties).forEach(([key, propertyStaticValues]) => {
     // Find the metric from this functions parameter.
     const metric = metrics.find((metric) => metric.name === key);
+
+    if (
+      ["snapshot_count", "most_recent_snapshot_age"].includes(key) &&
+      !metric?.facets?.length &&
+      typeof metric?.value === "undefined"
+    ) {
+      console.log(
+        `Metric ${key} not found. This could be OK, if the server has no snapshots.`,
+      );
+      return;
+    }
 
     if (!metric?.facets?.length && typeof metric?.value === "undefined") {
       console.log(metric);
